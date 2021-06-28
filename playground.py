5ff4e9f590750ab1e496fb228e9a3360106389c7
Jake Yoon <jakeyoon@Jakes-MacBook-Pro.local>
2021-06-28 20:43:58 +0900

    testing on gcloud

--- playground.py
+++ playground.py
@@ -2,7 +2,7 @@ from datetime import datetime
 import alpaca_trade_api as tradeapi
 import pytz, requests, json, time
 import pandas as pd
-import market_day, datamine, gap, profile, kakao, performance
+import market_day, datamine, gap, profile, kakao, performance, telegram_bot
 
 start = datetime.now()
 
@@ -19,6 +19,11 @@ start = datetime.now()
 
 tickers = gap.scan(api, prev_closes)
 print(tickers)
+print(datetime.now() - start)
+start = datetime.now()
+
+gap.order(api, tickers)
+telegram_bot.send_message("Order Finished")
 
 # get runtime
 print(datetime.now() - start)
