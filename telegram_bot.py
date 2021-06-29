f4721e9123d4c5122e60b973f54411f1996f956a
Jake Yoon <jakeyoon@Jakes-MacBook-Pro.local>
2021-06-29 19:31:34 +0900

    added position command

--- telegram_bot.py
+++ telegram_bot.py
@@ -18,6 +18,23 @@ def send_message(text):
 def status(update: Update, context: CallbackContext):
     if str(update.message.chat_id) == profile.TELEGRAM_CHAT_ID:
         user = update.effective_user
+        api = tradeapi.REST(
+            profile.APCA_API_KEY_ID,
+            profile.APCA_API_SECRET_KEY,
+            profile.APCA_API_BASE_URL
+        )
+        account = api.get_account()
+
+        update.message.reply_text("Current Equity: ${}".format(round(float(account.equity),2)))
+        pl = float(account.equity) - float(account.last_equity)
+        pl_pct = round((pl / float(account.last_equity)) * 100, 2)
+        update.message.reply_text("Current P/L: ${}   {}%".format(round(pl, 2), round(pl_pct, 2)))
+        update.message.reply_text("Current Cash: ${}".format(round(float(account.cash), 2)))
+        update.message.reply_text("Current Buying Power: ${}".format(round(float(account.buying_power), 2)))
+
+def positions(update: Update, context: CallbackContext):
+    if str(update.message.chat_id) == profile.TELEGRAM_CHAT_ID:
+        user = update.effective_user
         update.message.reply_text("I'm checking with your broker!")
 
         api = tradeapi.REST(
@@ -40,8 +57,6 @@ def status(update: Update, context: CallbackContext):
         update.message.reply_text("Current P/L: ${}   {}%".format(round(pl, 2), round(pl_pct, 2)))
         update.message.reply_text("Current Cash: ${}".format(round(float(account.cash), 2)))
         update.message.reply_text("Current Buying Power: ${}".format(round(float(account.buying_power), 2)))
-
-
 
 def close(update: Update, context: CallbackContext):
     if str(update.message.chat_id) == profile.TELEGRAM_CHAT_ID:
@@ -66,6 +81,7 @@ def main():
     
     dispatcher.add_handler(CommandHandler("start", start))
     dispatcher.add_handler(CommandHandler("status", status))
+    dispatcher.add_handler(CommandHandler("positions", positions))
     dispatcher.add_handler(CommandHandler("close", close))
 
     updater.start_polling()
